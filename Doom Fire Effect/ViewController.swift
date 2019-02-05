b51dd33e165a22fe0bbcf5dee2b84ee398606482
Lucas Pelinzon <lucasmpelinzon@gmail.com>
2019-02-05 15:56:26 -0200

    Initial Commit

---
+++ Doom Fire Effect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Doom Fire Effect
+//
+//  Created by Lucas Pelinzon on 05/02/19.
+//  Copyright © 2019 Lucas Pelinzon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
